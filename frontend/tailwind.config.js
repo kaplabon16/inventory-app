ef7b10a0ad2785059783a224ad8c65b33c81453a
Kaushik Plabon <kaushikplabon@Kaushiks-Mac-Pro.local>
2025-08-27 21:50:51 +0600

    34

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
-  theme: { extend: {} },
-  darkMode: 'class',
-  plugins: []
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
